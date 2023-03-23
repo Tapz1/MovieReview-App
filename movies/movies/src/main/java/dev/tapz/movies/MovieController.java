2964b3233ac112333d7b0226bfd63e40303c7472
Tapz1 <chris@tapz.dev>
2023-03-23 05:34:15 -0400

    began new movie review app by creating a db in mongodb atlas and configuring the project using Spring

---
+++ movies/movies/src/main/java/dev/tapz/movies/MovieController.java
@@ -0,0 +1,22 @@
+package dev.tapz.movies;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+
+    @GetMapping
+    public ResponseEntity<String> getAllMovies(){
+        return new ResponseEntity<String>("All Movies!", HttpStatus.OK);
+    }
+}
+
+/*
+returning ResponseEntity<> is better because it returns the response code which is handy when working on a frontend app
+ */
---
+++ movies/movies/src/main/java/dev/tapz/movies/MovieService.java
@@ -0,0 +1,15 @@
+package dev.tapz.movies;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies(){
+     return null;
+    }
+}
